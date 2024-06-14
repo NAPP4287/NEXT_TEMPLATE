fa106b77353889bd7c55add130f4cab340e4b887
Lee Na Eun <bella@nhsco.xyz>
2024-06-14 17:59:36 +0900

    [feature] - tailwind.css 추가 성능 작업

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,42 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        white: "#ffffff",
+        black: "#1a1a1a",
+        red: {
+          main: "#d65c2f",
+          light: "#ff8f66",
+          med: "#eb6434",
+        },
+        green: {
+          main: "#00bd48",
+          light: "#6ee69c",
+          med: "#3ac26e",
+        },
+        primay: {
+          // 프로젝트별 primary 색상
+          main: "",
+          sub: "",
+        },
+        gray: {
+          light: "#dedede",
+          med: "#8c8c8c",
+          dark: "#4f4f4f",
+        },
+      },
+      fontFamily: {
+        sans: ["Graphik", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+      },
+      padding: {
+        sm: "8px",
+        md: "12px",
+        df: "16px",
+        lg: "20px",
+        xlg: "36px",
+      },
+      spacing: {},
     },
   },
   plugins: [],
